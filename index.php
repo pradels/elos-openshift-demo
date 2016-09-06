df04f236a9f8c7d77a925ce48b9e25a17194c95b
Lukas Stanek <ls@elostech.cz>
2016-09-06 16:33:20 +0200

    Pridanie sablony

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<h1><?php echo 'Ahoj' ?></h1>
+<?php
+  $db_service  = getenv('DATABASE_SERVICE_NAME');
+  $db_name     = getenv('DATABASE_NAME');
+  $db_username = getenv('DATABASE_USER');
+  $db_password = getenv('DATABASE_PASSWORD');
+  $message     = getenv('HELLO_MESSAGE');
+?>
+
+<h1><?php echo "$message" ?></h1>
+
+service = <?php echo "$db_service" ?><br/>
+name = <?php echo "$db_name" ?><br/>
+username = <?php echo "$db_username" ?><br/>
+password = <?php echo "$db_password" ?><br/>
+
